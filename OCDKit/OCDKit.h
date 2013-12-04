1d52daf9f3e7806a21936ce7b9b5afb32bf962e9
Jeremy Carbaugh <jcarbaugh@gmail.com>
2013-12-04 00:33:31 -0500

    Initial Commit

---
+++ OCDKit/OCDKit.h
@@ -0,0 +1,13 @@
+//
+//  OCDKit.h
+//  OCDKit
+//
+//  Created by Jeremy Carbaugh on 12/4/13.
+//  Copyright (c) 2013 Jeremy Carbaugh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OCDKit : NSObject
+
+@end
---
+++ OCDKit/OCDKit.m
@@ -0,0 +1,13 @@
+//
+//  OCDKit.m
+//  OCDKit
+//
+//  Created by Jeremy Carbaugh on 12/4/13.
+//  Copyright (c) 2013 Jeremy Carbaugh. All rights reserved.
+//
+
+#import "OCDKit.h"
+
+@implementation OCDKit
+
+@end
