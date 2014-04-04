dbcc478c0b26bef7c99634ecc48e48f62e854c7f
Daniel Cloud <dcloud@sunlightfoundation.com>
2014-04-04 17:40:00 -0400

    Finish demo setup stuff. I guess direct link to container projects is okay? That's how people are doing it.

--- OCDKitDemo/Classes/OCDAppDelegate.m
+++ OCDKitDemo/Classes/OCDAppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "OCDAppDelegate.h"
+#import "OCDKit.h"
 
 @implementation OCDAppDelegate
 
@@ -16,6 +17,9 @@
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+
+    OCDClient *client = [OCDClient clientWithKey:@"foo"];
+
     return YES;
 }
 
