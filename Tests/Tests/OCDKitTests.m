10ebe056e0acf5c943907a5405239f41574020fb
Daniel Cloud <dcloud@sunlightfoundation.com>
2014-04-22 13:18:27 -0400

    Get OCDKit imported properly and write one silly test.

--- Tests/Tests/OCDKitTests.m
+++ Tests/Tests/OCDKitTests.m
@@ -1,6 +1,6 @@
 //
 //  OCDKitTests.m
-//  iOS Tests
+//  OCDKitTest
 //
 //  Created by Daniel Cloud on 4/22/14.
 //
@@ -8,20 +8,20 @@
 
 #import <XCTest/XCTest.h>
 
+#import "OCDClient.h"
+
 @interface OCDKitTests : XCTestCase
 
 @end
 
 @implementation OCDKitTests
 
-- (void)setUp
-{
+- (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
-- (void)tearDown
-{
+- (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
@@ -29,6 +29,11 @@
 - (void)testExample
 {
     XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+- (void)testClient {
+    OCDClient *client = [OCDClient clientWithKey:@"foo"];
+    XCTAssertEqual(client.baseURL.absoluteString , @"https://api.opencivicdata.org");
 }
 
 @end
