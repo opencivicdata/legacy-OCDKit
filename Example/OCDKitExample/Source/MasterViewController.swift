6c3bf1cc3927f8fd2826c29f3f69dcb1fc250480
ekstasis <straightstory@gmail.com>
2016-05-11 14:54:15 -0400

    Make MasterVC easier to diff

--- Example/OCDKitExample/Source/MasterViewController.swift
+++ Example/OCDKitExample/Source/MasterViewController.swift
@@ -9,70 +9,69 @@
 import UIKit
 
 class MasterViewController: UITableViewController {
-   
-   var detailViewController: DetailViewController? = nil
-   let types = [OCDType.Bill, OCDType.Division, OCDType.Event, OCDType.Jurisdiction, OCDType.Person, OCDType.Vote]
-   
-   
-   override func awakeFromNib() {
-      super.awakeFromNib()
-      if UIDevice.currentDevice().userInterfaceIdiom == .Pad {
-         self.clearsSelectionOnViewWillAppear = false
-         self.preferredContentSize = CGSize(width: 320.0, height: 600.0)
-      }
-   }
-   
-   override func viewDidLoad() {
-      super.viewDidLoad()
-      // Do any additional setup after loading the view, typically from a nib.
-      if let split = self.splitViewController {
-         let firstNavVC = split.viewControllers[0] as! UINavigationController
-         self.detailViewController = firstNavVC.topViewController as? DetailViewController
-         
-      }
-   }
-   
-   override func didReceiveMemoryWarning() {
-      super.didReceiveMemoryWarning()
-      // Dispose of any resources that can be recreated.
-   }
-   
-   // MARK: - Segues
-   
-   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-      if segue.identifier == "showDetail" {
-         if let indexPath = self.tableView.indexPathForSelectedRow {
-            let type = types[indexPath.row] as OCDType
-            let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
-            controller.detailType = type
-            controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
-            controller.navigationItem.leftItemsSupplementBackButton = true
-         }
-      }
-   }
-   
-   // MARK: - Table View
-   
-   override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-      return 1
-   }
-   
-   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-      return self.types.count
-   }
-   
-   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-      let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
-      
-      let type = types[indexPath.row] as OCDType
-      cell.textLabel?.text = type.label()
-      return cell
-   }
-   
-   override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-      // Return false if you do not want the specified item to be editable.
-      return false
-   }
-   
+
+    var detailViewController: DetailViewController? = nil
+    let types = [OCDType.Bill, OCDType.Division, OCDType.Event, OCDType.Jurisdiction, OCDType.Person, OCDType.Vote]
+
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        if UIDevice.currentDevice().userInterfaceIdiom == .Pad {
+            self.clearsSelectionOnViewWillAppear = false
+            self.preferredContentSize = CGSize(width: 320.0, height: 600.0)
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        if let split = self.splitViewController {
+            let firstNavVC = split.viewControllers[0] as! UINavigationController
+            self.detailViewController = firstNavVC.topViewController as? DetailViewController
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Segues
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showDetail" {
+            if let indexPath = self.tableView.indexPathForSelectedRow {
+                let type = types[indexPath.row] as OCDType
+                let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
+                controller.detailType = type
+                controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
+                controller.navigationItem.leftItemsSupplementBackButton = true
+            }
+        }
+    }
+
+    // MARK: - Table View
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.types.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+
+        let type = types[indexPath.row] as OCDType
+        cell.textLabel?.text = type.label()
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return false
+    }
+
 }
 
